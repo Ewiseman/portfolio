55da6cd4d04d239d8e54a8f299616b84f07823ce
Eric Wiseman <ewiseman1.com>
2019-01-05 12:43:25 -0700

    clean up index page

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,5 +3,9 @@ class Recipe < ApplicationRecord
   has_many :ingredients, through: :measurements, dependent: :destroy
   default_scope { order('name ASC') }
 
+  def multiplier_view
+    return " x #{multiplier}" if multiplier > 1
+  end
+
 
 end
