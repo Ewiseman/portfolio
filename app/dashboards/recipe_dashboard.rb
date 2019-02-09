2339e7f2ae019ec99c8319c7bc6d0aa3306ca042
Eric Wiseman <ewiseman1.com>
2019-02-09 12:46:31 -0700

    update recipes

--- app/dashboards/recipe_dashboard.rb
+++ app/dashboards/recipe_dashboard.rb
@@ -48,7 +48,7 @@ class RecipeDashboard < Administrate::BaseDashboard
   # By default, it's limited to four items to reduce clutter on index pages.
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = [
-    :id,
+    # :id,
     :on_the_menu,
     :name,
     :protein,
