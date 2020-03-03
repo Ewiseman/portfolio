98d2770af99fbd239efdf02e2130669cdf3444f0
Eric Wiseman <ewiseman1.com>
2020-03-03 15:01:03 -0700

    update index page

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -1,5 +1,9 @@
 class ParticipantsController < ApplicationController
 
+  def index
+    @hello = Participant.select(:first_name,:last_name).group(:first_name,:last_name).having("count(*) > 1")
+  end
+
   def show
     @participant = Participant.find(params[:id])
     # authorize @participant
