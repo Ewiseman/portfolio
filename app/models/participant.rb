4f40d19ac60993bc38929022bca7c4840dcb0b7a
Eric Wiseman <ewiseman1.com>
2020-03-03 10:44:00 -0700

    updated data table

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -1,4 +1,6 @@
 class Participant < ApplicationRecord
   has_many :locations, dependent: :destroy
   has_many :tags, through: :locations, dependent: :destroy
+
+  default_scope { order('last_name ASC') }
 end
