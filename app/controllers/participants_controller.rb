f259b8114eebcaf2d59c456f6f115da07241a798
Eric Wiseman <ewiseman1.com>
2020-03-26 17:30:43 -0600

    update particiapnts controller to find duplicates with emails

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -1,7 +1,20 @@
 class ParticipantsController < ApplicationController
 
   def index
-    @duplicates = Participant.select([:first_name,:last_name]).group(:first_name,:last_name).having("count(*) > 1").size
+    # @duplicates = (Participant.group(:first_name,:last_name)
+
+
+
+
+
+
+
+    # @duplicates = @duplicates.having("count(*) > 1").size
+
+
+    @duplicates = Participant.all.select([:first_name,:last_name, :email]).group(:first_name,:last_name).having("count(*) > 1").size
+
+
     # authorize @participants
   end
 
